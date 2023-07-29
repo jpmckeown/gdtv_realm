2865f9e7aa8d1a7aa76f474d5465fa22587dc180
J Patrick McKeown <jpmckeown@users.noreply.github.com>
2023-07-29 20:17:10 +0100

    gradual move using LERP, camera change to orthographic

--- Assets/Scripts/EnemyMover.cs
+++ Assets/Scripts/EnemyMover.cs
@@ -17,8 +17,16 @@ public class EnemyMover : MonoBehaviour
         foreach(Waypoint waypoint in path)
         {
             Debug.Log(waypoint.name);
-            transform.position = waypoint.transform.position;
-            yield return new WaitForSeconds(stepDelay);
+            Vector3 startPosition = transform.position;
+            Vector3 endPosition = waypoint.transform.position;
+            float travelPercent = 0f;
+
+            while(travelPercent < 1f)
+            {
+                travelPercent += Time.deltaTime;
+                transform.position = Vector3.Lerp(startPosition, endPosition, travelPercent);
+                yield return new WaitForEndOfFrame();
+            }
         }
     }
 }
