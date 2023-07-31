0f31f76d7f9c8fae1d6a1a2899d9c303bb69142e
J Patrick McKeown <jpmckeown@users.noreply.github.com>
2023-07-31 18:01:19 +0100

    rough tower prefab and code to place

--- Assets/Scripts/Waypoint.cs
+++ Assets/Scripts/Waypoint.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class Waypoint : MonoBehaviour
 {
     [SerializeField] bool canPlaceTower;
+    [SerializeField] GameObject towerPrefab;
 
     void OnMouseDown()
     {
         if(canPlaceTower) {
             Debug.Log($"{transform.name} available for tower placement");
+            Instantiate(towerPrefab, transform.position, Quaternion.identity);
         }
         else {
             Debug.Log($"Cannot make a tower here at {transform.name}");
