b52a810374f64b279f558b664e6e61fde43e5bc5
J Patrick McKeown <jpmckeown@users.noreply.github.com>
2023-08-02 08:33:51 +0100

    range and disabling projectile doesnt work yet

--- Assets/Scripts/TargetFind.cs
+++ Assets/Scripts/TargetFind.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class TargetFind : MonoBehaviour
 {
     [SerializeField] Transform weapon;
+    [SerializeField] ParticleSystem projectilePfx;
+    [SerializeField] float range = 15f;
     [SerializeField] Transform target;
 
     void Update()
@@ -36,6 +38,23 @@ public class TargetFind : MonoBehaviour
 
     void AimWeapon()
     {
+        float targetDistance = Vector3.Distance(transform.position, target.position);
+        
         weapon.LookAt(target);
+        
+        if(targetDistance < range)
+        {
+            Attack(true);
+        } 
+        else {
+            Attack(false);
+        }
+        Debug.Log($"{targetDistance} {range}");
+    }
+
+    void Attack(bool isPrimed)
+    {     
+        var emissionModule = projectilePfx.emission;
+        emissionModule.enabled = isPrimed;
     }
 }
